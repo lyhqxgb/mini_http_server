0da9eb95be9098e68b9a0a8798737d8797c70dcf
lyhqxgb <lyh52xx@126.com>
2019-04-18 23:48:04 +0800

    get method and response error 501

--- http_server.c
+++ http_server.c
@@ -5,6 +5,7 @@
 #include <sys/types.h>
 #include <arpa/inet.h>
 #include <errno.h>
+#include <ctype.h>
 
 #define SERVER_PORT 80
 
@@ -15,6 +16,10 @@ void print_err(char *msg)
 }
 
 int get_head_line(int socket, char *buff, int len);
+
+int get_method(char *line, char * method, unsigned int len);
+
+void request_501(int client_socket);
 
 int main(void)
 {
@@ -45,20 +50,33 @@ int main(void)
     while(1){
         int client_socket = 0;
         struct sockaddr_in client_addr;
-        socklen_t addr_len;
+        socklen_t addr_len = sizeof(client_addr);
         char ip_addr[20] = {0};
-        char line[200];
+        char line[200]; //记录一行请求信息
+        char method[30]; //请求方法
 
+        memset(&client_addr, 0, sizeof(client_addr));
         client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &addr_len);
-        if(res != 0) print_err("accept");
+        if(res < 0) print_err("accept");
 
         //打印ip地址
         printf("ip: %s, port: %d\n", inet_ntop(AF_INET, &client_addr.sin_addr.s_addr, ip_addr, sizeof(ip_addr)), ntohs(client_addr.sin_port));
 
         //打印http请求
-        while(res = get_head_line(client_socket, line, (int)sizeof(line))){
-            printf("%s\n", line);
+        // while(res = get_head_line(client_socket, line, (int)sizeof(line))){
+        //     printf("%s\n", line);
+        // }
+
+        //读取请求方法
+        res = get_head_line(client_socket, line, (int)sizeof(line));
+        if(!res) print_err("no head line");
+        get_method(line, method, sizeof(method));
+        if(strcmp(method, "GET") == 0){
+            printf("it is!\n");
+        }else{
+            request_501(client_socket);
         }
+
 
         close(client_socket);
     }
@@ -103,4 +121,49 @@ int get_head_line(int socket, char *buff, int len)
     }
     
     return count;
+}
+
+
+/**
+ * 获取请求方法
+ */
+int get_method(char *line, char * method, unsigned int len)
+{
+    int i;
+    char curr;
+    int count = 0;
+
+    //清空buff
+    memset(method, '\0', len);
+
+    for(i = 0; i < len; i++){
+        count++;
+        curr = line[i];
+        if(curr == '\r' || curr == '\n') continue;
+        if(curr == ' ') break;
+        method[i] = toupper(curr);
+    }
+
+    return count;
+}
+
+void request_501(int client_socket)
+{
+    int count;
+    const char *res = "HTTP/1.0 501 Method Not Implemented\r\n\
+Content-Type: text/html\r\n\
+\r\n\
+<HTML>\
+<HEAD>\
+<TITLE>Method Not Implemented</TITLE>\
+</HEAD>\
+<BODY>\
+    <P>HTTP request method not supported.\
+</BODY>\
+</HTML>";
+
+    count = write(client_socket, res, strlen(res));
+
+    printf("count: %d\n", count);
+    if(!count) print_err("write 501");
 }
